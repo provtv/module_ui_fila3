85b0eef7b3b1963c46321afb4e17cd6df3828294
marco xot <marco.sottana@gmail.com>
2025-03-26 09:30:17 +0100

    .

---
+++ lang/it/group.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'fields' => 
+  array (
+    'lavoratore' => 
+    array (
+      'label' => 'lavoratore',
+    ),
+    'qua' => 
+    array (
+      'label' => 'qua',
+    ),
+    'rep' => 
+    array (
+      'label' => 'rep',
+    ),
+    'periodo' => 
+    array (
+      'label' => 'periodo',
+    ),
+  ),
+);
