3b0e0942e4853ab82b078a78e429ad12142f05e1
marco76tv <marco.sottana@gmail.com>
2025-04-15 10:13:08 +0200

    .

---
+++ app/Filament/Resources/Pages/BaseListRecords.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\app\Filament\Resources\Pages;
+
+use Modules\UI\app\Traits\TableLayoutTrait;
+
+use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+
+abstract class BaseListRecords extends XotBaseListRecords
+{
+   
+} 
